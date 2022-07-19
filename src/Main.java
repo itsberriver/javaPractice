697c31127fc693a84239988723c15c3d8ef915e2
itsberriver <jmejiaos713@gmail.com>
2022-07-19 12:11:48 +0200

    created an options array and printed them in console

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,32 @@
+import java.util.Scanner;
+
 class HelloWorld {
     public static void main (String[] args) {
-        int[] numeros = {2,5,7,9,2};
-        int suma = 0;
+        Scanner input = new Scanner(System.in);
 
-        for (int numero: numeros){
-            suma+= numero;
-        }
-        System.out.println(suma);
+        String[] option = new String [3];
+        option[0] = "0.comprar producto";
+        option[1] = "1.añadir producto";
+        option[2] = "2.eliminar producto";
 
-        suma =0;
-        for (int i=0;i<numeros.length;i++){
-            suma+=numeros[i];
-        }
-        System.out.println(suma);
+
+        System.out.println("Digite el número de la opción que quiera realizar: " + option[0]+ "   "+ option[1]+ "   "+option[2]);
+        int choosedOption;
+        choosedOption = input.nextInt();
+
+        System.out.println("tu decisión ha sido: " + choosedOption );
+
+       /* System.out.println("tu decisión ha sido: " + option[0] );
+        System.out.println("tu decisión ha sido: " + option[2] );
+        System.out.println("tu decisión ha sido: " + option[3] );
+        System.out.println("esta opción no existe, vuelva a intentarlo:");*/
+        choosedOption = input.nextInt();
+
+
+
+
+
+
     }
 }
 
