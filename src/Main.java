c52e082eaf4acdfa633244e7407b0ada3e9806b6
itsberriver <jmejiaos713@gmail.com>
2022-07-19 13:01:50 +0200

    created an arrayList named productsList

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
 
 class HelloWorld {
     public static void main (String[] args) {
@@ -10,10 +10,14 @@ class HelloWorld {
         options[1] = "1.Añadir producto";
         options[2] = "2.Eliminar producto";
 
+        ArrayList<String> productsList= new ArrayList<>();
         String[] products = new String [3];
-        products[0] = "0.aguacate-2€";
-        products[1] = "1.leche-1€";
-        products[2] = "2.mantequilla-3€";
+        productsList.add ("0.aguacate-2€");
+        productsList.add ("1.leche-1€");
+        productsList.add ("2.mantequilla-3€");
+
+
+
 
 
         System.out.println("Digite el número de la opción que quiera realizar: ");
@@ -26,9 +30,7 @@ class HelloWorld {
         if ( choosedOption == 0){
             System.out.println("tu decisión ha sido: " + options[0] );
             System.out.println("Cuál de los siguientes productos quiere comprar?: " );
-            System.out.println( products[0]);
-            System.out.println( products[1]);
-            System.out.println( products[2]);
+            System.out.println( productsList);
             int choosedProduct;
             choosedProduct = input.nextInt();
             System.out.println("Producto comprado con éxito" );
@@ -36,9 +38,12 @@ class HelloWorld {
 
        if  ( choosedOption == 1){
             System.out.println("tu decisión ha sido: " + options[1] );
+            System.out.println("Inserte el nombre y precio del nuevo producto, separado por un guión:");
+
         }
         if  ( choosedOption == 2){
             System.out.println("tu decisión ha sido: " + options[2] );
+
         }
         if  ( choosedOption > 2){
             System.out.println("Cuidado! No exite esta opción, introduzca de nuevo la opción que desea: " );
@@ -47,17 +52,6 @@ class HelloWorld {
             System.out.println( options[2]);
             choosedOption = input.nextInt();
         }
-
-
-
-
-
-
-
-
-       /*
-        System.out.println("esta opción no existe, vuelva a intentarlo:");*/
-
 
 
 
